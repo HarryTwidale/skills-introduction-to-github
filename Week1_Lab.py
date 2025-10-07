ab2bf56542a572a4e642819d88c4337475623f2b
HarryTwidale <harrytwidale1@gmail.com>
2025-10-07 15:15:06 +0100

    Add files via upload

---
+++ Week1_Lab.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+student_name = "Harry"
+student_age = "18"
+
+print("Student Name:",student_name)
+print("Student Age:",student_age)
+
+num_1 = 10
+num_2 = 15
+total = num_1 + num_2
+print("The sum is:",total)
